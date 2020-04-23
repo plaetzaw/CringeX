38393d9d256f1c56d8d51133dfca7e396c3fc0a4
jaye-j <jacobrobertjensen@gmail.com>
2020-04-23 14:19:33 -0500

    fixed conflicts

--- Client/App.js
+++ Client/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import HomeScreen from "./src/components/Home";
+import LoginScreen from "./src/components/Login";
 import AccountScreen from "./src/components/Account";
 
 // function HomeScreen() {
@@ -20,7 +20,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Account" component={AccountScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Logo from "./src/components/Logo";
+import Logo from "./Logo";
 import LoginScreen from "react-native-login-screen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LoginScreen
-        source={require("./assets/cringebackground.png")}
+        source={require("../../assets/cringebackground.png")}
         logoComponent={<Logo />}
       />
     </View>

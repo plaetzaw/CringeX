831a264f1fa83559c64830ef4785edcd03824290
Matt Ryan <matthewtryan99@gmail.com>
2020-04-27 11:16:31 -0500

    elephantSQL starting new database

--- Server/App.js
+++ Server/App.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(require("./routes/login"));
+app.use(require("./routes/createUser"))
+
 app.listen(3001, ()=>{
     console.log("listening on port 3001")
 })
--- Server/routes/login.js
+++ Server/routes/login.js
@@ -1,7 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require('body-parser');
+let db = require('../models');
+let bcrypt = require('bcrypt');
 
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post("/", (req, res)=>{
-    
+router.post("/login", (req, res)=>{
+    let email = req.body.email;
+    let password = req.body.password;
+
+    db.user.findOne({
+        where: {
+            email : email
+        }
+    })
+    .then(currentUser =>{
+        if(currentUser){
+            bcrypt.compare(password, currentUser.password)
+            .then(correct => {
+                if(correct){
+                    console.log("user logged in")
+                } else {
+                    console.log("password is incorrect")
+                }
+            })
+        } else {
+            console.log("no user found")
+        }
+    })
+})
+
+router.get('/register', (req, res)=>{
+    res.render("users.ejs")
 })

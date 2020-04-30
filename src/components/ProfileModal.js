38cc3e9957bd07ef28247d771cf33cb7ef8d1e8e
jaye-j <jacobrobertjensen@gmail.com>
2020-04-30 15:58:22 -0500

    edit profile modal setup

---
+++ src/components/ProfileModal.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Button from "@material-ui/core/Button";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    width: "50%",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 2, 2, 2),
+  },
+}));
+
+export default function SimpleModal() {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <forum>
+        <h2>Edit Profile</h2>
+        <hr />
+        <div>
+          Bio: <input></input>
+        </div>
+        <hr />
+        <div>
+          Profile Image:{" "}
+          <Button variant="contained" color="primary">
+            Upload Image
+          </Button>
+        </div>
+      </forum>
+    </div>
+  );
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          type="button"
+          onClick={handleOpen}
+        >
+          <SettingsIcon />
+        </Button>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+    </div>
+  );
+}

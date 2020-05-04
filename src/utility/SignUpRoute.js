800b07ad190dbb543cbee536a84fc4dd42e3f8d2
jaye-j <jacobrobertjensen@gmail.com>
2020-05-04 10:24:40 -0500

    fixed signup route

--- src/utility/SignUpRoute.js
+++ src/utility/SignUpRoute.js
@@ -3,16 +3,16 @@ import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-const SignUpRoute = ({ component: Component, loggedIn, ...rest }) => (
+const SignUpRoute = ({ component: Component, signedUp, ...rest }) => (
   <Route
     {...rest}
     render={(props) =>
-      rest.status === 200 ? <Redirect to="/" /> : <Component {...props} />
+      signedUp === true ? <Redirect to="/" /> : <Component {...props} />
     }
   />
 );
 const mapStateToProps = (state) => ({
-  loggedIn: state.Users.loggedIn,
+  signedUp: state.Users.signedUp,
 });
 
 SignUpRoute.propTypes = {

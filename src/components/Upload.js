f9d10980dc5c41b2fdfdfbc61381d7bb338b6a8a
plaetzaw <plaetzaw@gmail.com>
2020-05-03 21:45:33 -0500

    Switch Statement Wroks now yay

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -49,6 +49,10 @@ class ImageUpload extends Component {
       () => {
         // complete function ...
         console.log("Saving to storage...");
+
+        console.log(image);
+        console.log(image.name);
+        // cannot console log in a function chain like that below
         storage
           .ref("images")
           .child(image.name)
@@ -58,34 +62,54 @@ class ImageUpload extends Component {
             //make api call here to post information back to the server
             //pseudo code
             console.log("Posting to server...");
+            console.log(this.state.url);
             let type = "";
-            let media = this.state.url.split(".");
+            // let media = this.state.url.split(".");
+            let firebaseURL = this.state.url;
+            let splitURL = firebaseURL.split("?");
+
+            // https://firebasestorage.googleapis.com/v0/b/cringex-46ee6.appspot.com/o/images%2Fa2.jpg
+            //?
+            //alt=media&token=080a925d-bb17-41ac-b1c5-1c083c90ecc6
+
+            // splitURL = ["https://firebasestorage.googleapis.com/v0/b/cringex-46ee6.appspot.com/o/images%2Fa2.jpg", "alt=media&token=080a925d-bb17-41ac-b1c5-1c083c90ecc6"]
+
+            let splitPeriod = splitURL[0];
+
+            let media = splitPeriod.split(".");
+            console.log("Testing split URL");
+            console.log(splitPeriod);
+            console.log(media);
+            console.log(media[media.length - 1]);
             //last element in the array  media[-1]
 
             //split on (.) i.e myImage.jpg
             //switch on the media[-1]
-            switch (media[-1]) {
+            switch (media[media.length - 1]) {
               case "jpg" || "png" || "jpeg" || "gif":
                 type = "image";
                 break;
               case "mp4" || "mp5" || "flv" || "mpeg":
                 type = "video";
                 break;
+              default:
+                console.log("UNSUPPORTED FILE TYPE");
+                break;
             }
             console.log(type);
             console.log(this.state.url);
             // console.log(this.state.caption);
 
-            axios
-              .post("/upload", {
-                videoUrl: this.state.url,
-                type: type,
-                caption: this.state.caption,
-              })
-              .then((response) => {
-                console.log(response);
-                console.log("information submitted to database");
-              });
+            let apiPayload = {
+              videoUrl: this.state.url,
+              postType: "image-test",
+              caption: this.state.caption,
+            };
+
+            axios.post("/upload", apiPayload).then((response) => {
+              console.log(response);
+              console.log("information submitted to database");
+            });
           });
       }
     );

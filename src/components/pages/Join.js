d4a75ff185de1e9fce0b126a77c1209e027452d8
Daniela <daniela.arroyo27@gmail.com>
2020-05-04 08:05:24 -0500

    imp css base

--- src/components/pages/Join.js
+++ src/components/pages/Join.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Join.css'
 const Join = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
@@ -8,12 +9,12 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
+                {/* <h1 className="heading">Join</h1> */}
                 <div><input placeholder="" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
                 <div><input placeholder="" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
-                <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&roomm=${room}`}>
-                    <button className="button mt-20" type="submit"> Sign Up </button>
-                </Link >
+                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className={'button mt-20'} type="submit">Chat Now</button>
+                </Link>
             </div>
         </div>
     )

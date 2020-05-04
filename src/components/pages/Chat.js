22ed5b7fa4d483b1b53c3f4cf2c330c7a2df103c
Daniela <daniela.arroyo27@gmail.com>
2020-05-04 08:36:07 -0500

    client side io

--- src/components/pages/Chat.js
+++ src/components/pages/Chat.js
@@ -1,10 +1,36 @@
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
+import io from "socket.io-client";
 
-import React from 'react';
+import './Chat.css';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost: 8081'
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name)
+        setRoom(room)
+
+        socket.emit('join', { name, room });
+
+    }, [ENDPOINT, location.search]);
+
+
+
+
+
     return (
-        <h1>Chat</h1>
-    )
+        <div className="outerContainer">
+        </div>
+    );
 }
 
 export default Chat;
